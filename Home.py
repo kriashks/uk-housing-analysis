6fe52bf57c37ff1037c9f360be8ba1fe2a85e185
Adarsh Krishnan <kriashks@gmail.com>
2023-03-03 23:38:09 +0000

    Update Home.py
    Adding a hello world to test streamlit cloud deployment.

--- Home.py
+++ Home.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+name = st.text_input("Please, enter your name.", value="John")
+
+if name:
+  string = f"Hello, {name}!"
+  st.write(string)
+else:
+  st.write("Hello, stranger!")
